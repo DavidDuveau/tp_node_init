8609836966dedce31f1602af76c5fd333f629092
DavidDuveau <david.duveau@hotmail.fr>
2020-10-22 11:39:54 +0200

    first branch commit

--- src/import/connector.js
+++ src/import/connector.js
@@ -11,25 +11,25 @@ const connector = mysql.createConnection({
 connector.connect((err) => {
   if (err) throw err;
   console.log("Connected!");
-  connector.query(
-    `SELECT SCHEMA_NAME
-      FROM INFORMATION_SCHEMA.SCHEMATA
-     WHERE SCHEMA_NAME = ${databaseName}
-    `,
-    (err, result, field) => {
-      if (err) throw err;
-      if (result.length) {
-        console.log(`Database : ${databaseName} already created`);
-        loadES6Data();
-      } else {
-        connector.query(`CREATE DATABASE ${databaseName}`, (err, result) => {
-          if (err) throw err;
-          console.log(`Database created`);
-          loadES6Data();
-        });
-      }
-    }
-  );
+  // connector.query(
+  //   `SELECT SCHEMA_NAME
+  //     FROM INFORMATION_SCHEMA.SCHEMATA
+  //    WHERE SCHEMA_NAME = ${databaseName}
+  //   `,
+  //   (err, result, field) => {
+  //     if (err) throw err;
+  //     if (result.length) {
+  //       console.log(`Database : ${databaseName} already created`);
+  //       loadES6Data();
+  //     } else {
+  //       connector.query(`CREATE DATABASE ${databaseName}`, (err, result) => {
+  //         if (err) throw err;
+  //         console.log(`Database created`);
+  //         loadES6Data();
+  //       });
+  //     }
+  //   }
+  // );
 });
 
 export default connector;
